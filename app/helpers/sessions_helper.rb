6acf420b349b864828231f7da20927cd1a4f1382
jfeng <jfeng@thoughtworks.com>
2012-12-14 00:16:30 +0800

    add remember_token to session too, in order to handle cookies not allowed in browser

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,6 @@
 module SessionsHelper
   def sign_in(user)
+    session[:remember_token] = user.remember_token
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
@@ -13,6 +14,7 @@ module SessionsHelper
   end
 
   def current_user
+    @current_user ||= User.find_by_remember_token(session[:remember_token])
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
@@ -22,6 +24,7 @@ module SessionsHelper
 
   def sign_out
     self.current_user = nil
+    session.delete(:remember_token)
     cookies.delete(:remember_token)
   end
 end
